3b05235e36669d97d87e4c8254de7c7374ee5c49
MatthewZSmall <132171185+MatthewZSmall@users.noreply.github.com>
2023-04-30 12:09:46 -0600

    Update Part_II.sql

--- Part_II.sql
+++ Part_II.sql
@@ -8,15 +8,17 @@ WITH ordered AS (
     as_of_date,
     portfolio,
     holding,
-    -- return 1 for all holdings for the max as_of_date per portfolio
-    RANK() OVER(PARTITION BY portfolio ORDER BY as_of_date DESC) AS rn
+    other_fields,
+    -- returns 1 for all holdings for the max as_of_date per portfolio
+    RANK() OVER(PARTITION BY portfolio ORDER BY as_of_date DESC) AS rnk
   FROM
     table)
 SELECT
   as_of_date,
   portfolio,
-  holding
+  holding,
+  other_fields
 FROM
   ordered
 WHERE
-  rn = 1;
+  rnk = 1;
